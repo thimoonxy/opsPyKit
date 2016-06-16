716327403a30544aceaa3f14cd39b51769045ac3
thimoonxy <thimoon@sina.com>
2016-06-16 14:57:27 +0800

    fix ip2network_address, cidr2mask, mask_complement in netie.py

--- netie.py
+++ netie.py
@@ -9,38 +9,38 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def subnet_correct(ip, postfix='32'):
+def ip2network_address(ip, cidr='32'):
     """
-    This func corrects wrong subnet, e.g.
+    This func also corrects wrong subnet, e.g.
     if found 10.69.231.70/29, it'll be corrected to 10.69.231.64/29.
     """
     ip = ip.split('.')
-    net = int(postfix)
-    fullnet = '0b11111111'
-    zeronet = '0b00000000'
-    if net <= 8:
-        hosts = 8 - net
-        net = '0b' + '1'* net + '0' * hosts
-        net = (net, zeronet, zeronet, zeronet)
-    elif 8 < net <= 16:
-        hosts = 16 - net
-        net = '0b' + '1'* net + '0' * hosts
-        net = (fullnet, net, zeronet, zeronet)
-    elif 16 < net <= 24:
-        hosts = 24 - net
-        net = '0b' + '1'* net + '0' * hosts
-        net = (fullnet, fullnet, net, zeronet)
-    else:
-        hosts = 32 - net
-        net = '0b' + '1'* net + '0' * hosts
-        net = (fullnet, fullnet, fullnet, net)
+    netmask, bin_mask_list = cidr2mask(cidr)
     for x in range(len(ip)):
-        # print ip[x], type(net[x])
-        ip[x] = int(ip[x]) & int(net[x], 2)
-    ip = "%s.%s.%s.%s" % (ip[0], ip[1], ip[2], ip[3])
-    return  ip
+        # print ip[x], int(bin_mask_list[x], 2)
+        ip[x] = int(ip[x]) & int(bin_mask_list[x], 2)
+    if int(cidr) < 31:
+        network_address = "%s.%s.%s.%s" % (ip[0], ip[1], ip[2], ip[3])
+        first_avail_ip = "%s.%s.%s.%s" % (ip[0], ip[1], ip[2], ip[3]+1)
+        avail_host_numbers = 2 ** (32 - int(cidr)) - 2
+        complement_mask, complement_bin_list = mask_complement(netmask)
+        broadcase_address = '.'.join([ str(ip[x]+int(complement_bin_list[x],2)) for x in range(4)])
+        last_avail_ip_list = broadcase_address.split('.')[0:3]
+        last_avail_ip_list.append( str( int(broadcase_address.split('.')[-1]) -1 ) )
+        last_avail_ip = '.'.join(last_avail_ip_list)
+    elif int(cidr) == 31:
+        broadcase_address = network_address = None
+        first_avail_ip = "%s.%s.%s.%s" % (ip[0], ip[1], ip[2], ip[3])
+        last_avail_ip = "%s.%s.%s.%s" % (ip[0], ip[1], ip[2], ip[3]+1)
+        avail_host_numbers = 2
+    else:
+        broadcase_address = network_address = None
+        first_avail_ip = "%s.%s.%s.%s" % (ip[0], ip[1], ip[2], ip[3])
+        last_avail_ip = first_avail_ip
+        avail_host_numbers = 1
+    return  avail_host_numbers, netmask, network_address, first_avail_ip,last_avail_ip, broadcase_address
 
-def mask2int(mask='255.255.255.0'):
+def mask2cidr(mask='255.255.255.0'):
     '''
     1.  '255.11111.266.4' to '255.255.255.0' to 24
     2.  '255.128.255.0'  to 9
@@ -59,19 +59,59 @@ def mask2int(mask='255.255.255.0'):
     zindex = binmask_total.index('0')
     return  zindex
 
+def cidr2mask(cidr='23'):
+    cidr=int(cidr)
+    fullnet = '0b11111111'
+    zeronet = '0b00000000'
+    if cidr <= 8:
+        hosts = 8 - cidr
+        net = '0b' + '1'* cidr + '0' * hosts
+        net = (net, zeronet, zeronet, zeronet)
+    elif 8 < cidr <= 16:
+        hosts = 16 - cidr
+        net = '0b' + '1'* (cidr-8) + '0' * hosts
+        net = (fullnet, net, zeronet, zeronet)
+    elif 16 < cidr <= 24:
+        cidr = cidr - 16
+        hosts = 8 - cidr
+        net = '0b' + '1'* cidr + '0' * hosts
+        net = (fullnet, fullnet, net, zeronet)
+    else:
+        cidr = cidr - 24
+        hosts = 8 - cidr
+        # print cidr,hosts
+        net = '0b' + '1'* cidr + '0' * hosts
+        net = (fullnet, fullnet, fullnet, net)
+    bin_mask_list = net
+    netmask = '.'.join([ str(int(net[x], 2)) for x in range(len(net)) ])
+    return netmask, bin_mask_list
+
+def mask_complement(mask='255.255.255.0'):
+    cidr = mask2cidr(mask)
+    netmask, bin_mask_list = cidr2mask(cidr)
+    xcomplement_bin_list = [x.split('0b')[1] for x in bin_mask_list]
+    anti = lambda x: (x == '1') and '0' or '1'
+    bin2mask = lambda bstr: str( int(bstr,2))
+    complement_bin_list = [  ''.join(map(anti,list(x))) for x in xcomplement_bin_list ]
+    complement_bin_list = [ '0b'+x for x in complement_bin_list]
+    complement_mask = '.'.join(map(bin2mask,complement_bin_list))
+    return complement_mask, complement_bin_list
+
+
 def help():
     print 'help info...'
     sys.exit(1)
 
 def main():
-    '''
-    ip = '10.69.231.70'
-    net = '29'
-    ip = subnet_correct(ip, net)
-    '''
-    # print mask2int('224.0.0.0')
-    # mask_correcter('255.128.255.0')
 
+    ip = '10.69.231.70'
+    net = 29
+    print ip2network_address(ip, net)
+    print cidr2mask(net)
+    print mask_complement(cidr2mask(net)[0])
+
+    '''
+    # cmd
     args = sys.argv
     net = args[1].split('/')
 
@@ -81,6 +121,6 @@ def main():
     else:                             # in form like 192.168.1.2/24
         net,mask = net
         print subnet_correct(net,mask)
-
+    '''
 if __name__ == '__main__':
     main()
